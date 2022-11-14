d673ed1c14cd69d6adaa0529f7e9e38844fc8143
Gaurav Singh <gaurav.s.140102@gmail.com>
2022-11-14 22:48:58 +0530

    Fix CSS and make app responsive

--- src/App.js
+++ src/App.js
@@ -7,34 +7,24 @@ function App() {
 				<h1 className="title">Smart Street Lighting System</h1>
 				<div className="frameContainer">
 					<iframe
-						width="450"
-						height="260"
-						style={{ border: "1px solid #cccccc" }}
-						src="https://thingspeak.com/channels/1914520/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+						className="frames"
+						src="https://thingspeak.com/channels/1914520/charts/1?width=auto&height=auto&bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
 					></iframe>
 					<iframe
-						width="450"
-						height="260"
-						style={{ border: "1px solid #cccccc" }}
-						src="https://thingspeak.com/channels/1914520/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+						className="frames"
+						src="https://thingspeak.com/channels/1914520/charts/2?width=auto&height=autobgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
 					></iframe>
 					<iframe
-						width="450"
-						height="260"
-						style={{ border: "1px solid #cccccc" }}
-						src="https://thingspeak.com/channels/1914520/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+						className="frames"
+						src="https://thingspeak.com/channels/1914520/charts/3?width=auto&height=autobgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
 					></iframe>
 					<iframe
-						width="450"
-						height="260"
-						style={{ border: "1px solid #cccccc" }}
-						src="https://thingspeak.com/channels/1914520/charts/5?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+						className="frames"
+						src="https://thingspeak.com/channels/1914520/charts/5?width=auto&height=autobgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
 					></iframe>
 					<iframe
-						width="450"
-						height="260"
-						style={{ border: "1px solid #cccccc" }}
-						src="https://thingspeak.com/channels/1914520/charts/6?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+						className="frames"
+						src="https://thingspeak.com/channels/1914520/charts/6?width=auto&height=autobgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
 					></iframe>
 				</div>
 			</div>
